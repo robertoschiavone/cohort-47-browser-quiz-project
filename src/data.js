137fb651bb60f2f527f709f5b32b3a0fb754d46a
Roberto Schiavone <hello@robertoschiavone.io>
2024-03-06 19:48:11 +0100

    Revert "added a favicon, a live score update" (#13)
    This reverts commit ed43e5217e78fae39e3e81b4ebfe4ac58841b29c.

--- src/data.js
+++ src/data.js
@@ -14,175 +14,47 @@ export const quizData = {
   // the questions in the quiz
   questions: [
     {
-      text: 'Who is the leader of the Avengers?',
+      text: 'What are the different ways to declare a JS variable?',
       answers: {
-        a: 'Iron Man',
-        b: 'Captain America',
-        c: 'Thor',
-        d: 'Black Widow',
+        a: 'constant, let, variable',
+        b: 'var, const, let, function',
+        c: 'var, let, const',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'What does `typeof` do?',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
       },
       correct: 'b',
       selected: null,
       links: [
         {
-          text: 'Marvel Cinematic Universe Wiki',
-          href: 'https://marvelcinematicuniverse.fandom.com/wiki/Avengers',
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
         },
-      ],
-    },
-    {
-      text: 'Which Infinity Stone does Vision possess?',
-      answers: {
-        a: 'Mind Stone',
-        b: 'Reality Stone',
-        c: 'Power Stone',
-        d: 'Space Stone',
-      },
-      correct: 'a',
-      selected: null,
-      links: [
         {
-          text: 'Marvel Database',
-          href: 'https://marvel.fandom.com/wiki/Vision',
-        },
-      ],
-    },
-    {
-      text: 'What is the real name of Black Widow?',
-      answers: {
-        a: 'Natasha Romanoff',
-        b: 'Wanda Maximoff',
-        c: 'Carol Danvers',
-        d: 'Maria Hill',
-      },
-      correct: 'a',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Cinematic Universe Wiki',
+          text: 'MDN',
           href:
-            'https://marvelcinematicuniverse.fandom.com/wiki/Natasha_Romanoff',
+            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
         },
       ],
     },
-    {
-      text: 'Which villain is known for wielding the Infinity Gauntlet?',
-      answers: {
-        a: 'Loki',
-        b: 'Red Skull',
-        c: 'Thanos',
-        d: 'Ultron',
-      },
-      correct: 'c',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Database',
-          href: 'https://marvel.fandom.com/wiki/Thanos',
-        },
-      ],
-    },
-    {
-      text: "What metal is Captain America's shield made of?",
-      answers: {
-        a: 'Adamantium',
-        b: 'Vibranium',
-        c: 'Uru',
-        d: 'Promethium',
-      },
-      correct: 'b',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Database',
-          href: 'https://marvel.fandom.com/wiki/Vibranium',
-        },
-      ],
-    },
-    {
-      text: 'Who is the Norse god of thunder in the Avengers?',
-      answers: {
-        a: 'Loki',
-        b: 'Odin',
-        c: 'Thor',
-        d: 'Hela',
-      },
-      correct: 'c',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Cinematic Universe Wiki',
-          href: 'https://marvelcinematicuniverse.fandom.com/wiki/Thor',
-        },
-      ],
-    },
-    {
-      text: 'Which Avenger can shrink and grow in size?',
-      answers: {
-        a: 'Ant-Man',
-        b: 'Hawkeye',
-        c: 'Falcon',
-        d: 'Doctor Strange',
-      },
-      correct: 'a',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Database',
-          href: 'https://marvel.fandom.com/wiki/Ant-Man',
-        },
-      ],
-    },
-    {
-      text: 'Who is known as the "Strongest Avenger"?',
-      answers: {
-        a: 'Hulk',
-        b: 'Thor',
-        c: 'Iron Man',
-        d: 'Captain America',
-      },
-      correct: 'a',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Cinematic Universe Wiki',
-          href: 'https://marvelcinematicuniverse.fandom.com/wiki/Hulk',
-        },
-      ],
-    },
-    {
-      text: 'Which Avenger is a master archer?',
-      answers: {
-        a: 'Hawkeye',
-        b: 'Black Panther',
-        c: 'Spider-Man',
-        d: 'Doctor Strange',
-      },
-      correct: 'a',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Cinematic Universe Wiki',
-          href: 'https://marvelcinematicuniverse.fandom.com/wiki/Hawkeye',
-        },
-      ],
-    },
-    {
-      text: "What is the name of Tony Stark's AI assistant?",
-      answers: {
-        a: 'J.A.R.V.I.S.',
-        b: 'Ultron',
-        c: 'F.R.I.D.A.Y.',
-        d: 'Siri',
-      },
-      correct: 'c',
-      selected: null,
-      links: [
-        {
-          text: 'Marvel Cinematic Universe Wiki',
-          href: 'https://marvelcinematicuniverse.fandom.com/wiki/F.R.I.D.A.Y.',
-        },
-      ],
-    },
+    // Add more questions here
   ],
 };
--- src/views/questionView.js
+++ src/views/questionView.js
@@ -10,7 +10,6 @@ export const createQuestionElement = (question) => {
 
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
-  
     <h1>${question}</h1>
 
     <ul id="${ANSWERS_LIST_ID}">
